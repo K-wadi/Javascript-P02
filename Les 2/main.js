2cad24f848b2a42f862857cfbba843a893ed3665
K-wadi <302652872@student.rocmondriaan.nl>
2023-11-14 15:21:32 +0100

    gefixed

--- Les 2/main.js
+++ Les 2/main.js
@@ -9,9 +9,10 @@ const person = {
 
 // Opdracht 2
 const post = {
-  Username: "JohnDoe123",
-  Title: "Een geweldige dag in het park!",
-  Story: "Vandaag heb ik genoten van een heerlijke dag in het park. Het weer was perfect, en ik heb zelfs een paar nieuwe vrienden gemaakt. 🌳☀️",
-  Date: "5 november 2023",
-  Views: 250,
+  userName: "JohnDoe123",
+  title: "Een geweldige dag in het park!",
+  story:
+    "Vandaag heb ik genoten van een heerlijke dag in het park. Het weer was perfect, en ik heb zelfs een paar nieuwe vrienden gemaakt. 🌳☀️",
+  date: "5 november 2023",
+  views: 250,
 };
