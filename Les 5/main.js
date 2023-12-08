0847b6ef1555b50d3a86c5368b6148b320c1eb9c
K-wadi <302652872@student.rocmondriaan.nl>
2023-12-08 10:24:55 +0100

    fetch

--- Les 5/main.js
+++ Les 5/main.js
@@ -1,14 +1,48 @@
 console.log(`main.js is running...`);
 
-const ul = document.querySelector("ul");
+const ul = document.querySelector(".productList");
 
-fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
-.then((response) => response.json())
-.then((data) => {
-    const results = data.results;
+fetch("https://dummyjson.com/products")
+  .then((response) => response.json())
+  .then((data) => {
+    const products = data.products;
 
-for (let i = 0; i < results.length; i++) {
-    const element = results[i];
-    ul.innerHTML += `<li>${element.name}</li>`;
+    for (let i = 0; i < products.length; i++) {
+      const element = products[i];
+      ul.innerHTML += createCard(element);
+    }
+  });
+
+function createCard(product) {
+  const {
+    id,
+    title,
+    description,
+    price,
+    discountPercentage,
+    rating,
+    stock,
+    brand,
+    category,
+    thumbnail,
+    images,
+  } = product;
+
+  const code = `
+    <div class="card">
+      <img src="${thumbnail}" alt="${title}" class="card-img">
+      <div class="card-body">
+        <h2 class="card-title">${title}</h2>
+        <p class="card-info">${description}</p>
+        <p class="card-price">$${price}</p>
+        <p class="card-discount">${discountPercentage}% Discount</p>
+        <p class="card-rating">Rating: ${rating}</p>
+        <p class="card-stock">Stock: ${stock}</p>
+        <p class="card-brand">Brand: ${brand}</p>
+        <p class="card-category">Category: ${category}</p>
+      </div>
+    </div>
+  `;
+
+  return code;
 }
-});
